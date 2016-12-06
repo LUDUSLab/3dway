198b32514550265428628aea687d5a8faeddb1bd
Lucasfrota <frota_lv@hotmail.com>
2016-12-06 10:58:31 -0400

    VRmonster

---
+++ VRmonster/Assets/Scripts/monsterBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class monsterBehaviour : MonoBehaviour {
+
+	public Animator anime;
+
+	private bool tomaDano;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(tomaDano == false){
+			anime.SetBool ("tomaDano", false);
+		}
+		if(tomaDano == true){
+			anime.SetBool ("tomaDano", true);
+			tomaDano = false;
+		}
+	}
+
+	void OnCollisionEnter(Collision other){
+		tomaDano = true;
+	}
+}
